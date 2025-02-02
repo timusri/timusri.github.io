c5eed346d6e200c606e67849d4f7ffc2c7d427b6
timusri <sumit.srivastava@invideo.io>
2025-02-02 06:15:18 +0530

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '',
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
+
